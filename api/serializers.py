f64618c5c19c5dea9f75f2779353b1bc6aa8b1e8
Zhongyi-Lu <lu.zhon@northeastern.edu>
2024-01-30 12:13:49 -0800

    feat: initialize password reset

--- api/serializers.py
+++ api/serializers.py
@@ -103,6 +103,20 @@ class RegisterSerializer(serializers.ModelSerializer):
         })
         send_mail( mail_subject, message, settings.EMAIL_HOST_USER, [user.email] )
         return user
+    
+
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate(self, value):
+        try:
+            email = value.get('email')
+            User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError(
+                "User with given email does not exist")
+        return value
+
 
 class Userializer(serializers.ModelSerializer):
     district = DistrictsSerializer()
