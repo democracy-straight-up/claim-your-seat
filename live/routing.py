b2d7241798d1c817798ff4e4a1b1cfb6abfb97de
Zaman Ehsani <zamanehsani@gmail.com>
2025-04-20 04:26:16 +0400

    Moda (#123)
    * added the DB schema and models for S-Link (MODA)
    
    * made the serializers for Moda and members. Added to admin file as well.
    
    * fixed the serializers name
    
    * added the urls for S-Link, views and more
    
    * finished the models file, views, urls and serializers classes
    
    * added the rest of the schema and DB. made the migrations as well
    
    * udpates and bug fixes
    
    * updates on some namae changes and routes for websocket protocals
    
    * fixed the models
    
    * updated the routing file on live app to match with lower case file naming

--- live/routing.py
+++ live/routing.py
@@ -2,12 +2,12 @@ from django.urls import re_path
 from live import consumers
 from live import consumerCircle
 from live import consumerSecDel
-from live import consumerModa
+from live import consumermoda
 
 websocket_urlpatterns = [
     re_path(r'ws/circle/(?P<circle_name>\w+)/(?P<user_name>\w+)/$',consumers.HouseKeepingConsumer.as_asgi()),
     re_path(r'circle/(?P<circle_name>\w+)/(?P<user_name>\w+)',consumerCircle.CircleConsumer.as_asgi()),
     re_path(r'ws/(?P<circleName>\w+)/(?P<userName>\w+)',consumers.CircleBackNForth.as_asgi()),
     re_path(r'sec-del/(?P<sec_del_name>\w+)/(?P<user_name>\w+)',consumerSecDel.SecDelConsumer.as_asgi()),
-    re_path(r'moda/(?P<moda_name>\w+)/(?P<user_name>\w+)',consumerModa.ModaConsumer.as_asgi()),
+    re_path(r'moda/(?P<moda_name>\w+)/(?P<user_name>\w+)',consumermoda.ModaConsumer.as_asgi()),
 ]
