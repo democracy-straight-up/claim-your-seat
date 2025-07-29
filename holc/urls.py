c8a8258079f15d90f22e51f9f2577c9a25922aa3
Zaman Ehsani <zamanehsani@gmail.com>
2025-07-29 21:14:07 +0400

    done with two view sets for holc and their associated endpoint and more

--- holc/urls.py
+++ holc/urls.py
@@ -5,10 +5,7 @@ from holc import views
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-# router.register('', views.HelloWorldView)
-# router.register('moda-members', views.ModaMembersViewSet)
+router.register('holc', views.HolcViewSet)
+router.register('holc-members', views.HolcMembersViewSet)
 
-urlpatterns = [
-    path('hello/', views.HelloWorldView.as_view(), name='hello-world'),
-    path('', include(router.urls)),
-    ]
+urlpatterns = [path('', include(router.urls)),]
--- holc/views.py
+++ holc/views.py
@@ -1,10 +1,79 @@
-from rest_framework.views import APIView
+from rest_framework import viewsets
+from holc import models 
+from holc import serializers
+from django.contrib.auth.models import User
+from vote.models import Districts
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import JsonResponse
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
 
+class HolcViewSet(viewsets.ModelViewSet):
+    queryset = models.HolcModel.objects.all()
+    serializer_class = serializers.HolcSerializer
+    
+    def create(self, request, *args, **kwargs):
+        try:
+            user=None, 
+            district =None
+            if 'user' in request.data and 'district' in request.data:
+                user = User.objects.get(username=request.data['user'])
+                district = Districts.objects.get(
+                    code=request.data['district'])
+            else:
+                messages = "District is required."
+                return Response({"message:": messages}, status=status.HTTP_400_BAD_REQUEST)
 
-class HelloWorldView(APIView):
-    def get(self, request):
-        return Response({"message": "hello world"}, status=status.HTTP_200_OK)
+            # check if the userType is not 0 return
+            if user.users.userType[:2] == 'U4':
+                messages = "Already belong to another Holc."
+                return Response({"message": messages}, status=status.HTTP_400_BAD_REQUEST)
+            # create a sec_del object
+            holc =  models.HolcModel.objects.create(district=district)
+    
+            # set the userType attribute of the creator to 1
+            # add the user to circle member as delegate. it does not require to save. create automatically saves as well
+            models.HolcMembers.objects.create(user=user, holc=holc, is_member=True)
+
+            obj = serializers.HolcSerializer(holc)
+    
+            return JsonResponse(obj.data)
+        except:
+            messages = "Something Went Wrong."
+            return Response({"message:": messages}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['POST'])
+    def get_holc_by_user(self,request):
+        try:
+            username = request.data['user']
+        except KeyError:
+            return Response({'message': "Missing 'user' field in request data."},
+                status=status.HTTP_400_BAD_REQUEST)
+        # get_object_or_404 handles the try/except DoesNotExist for you
+        # It raises Http404 if the object is not found
+        instance = get_object_or_404(models.HolcMembers, user__username=username)
+        # If the object is found, execution continues here
+        serial = self.get_serializer(instance.holc)
+        return Response(serial.data)
+    
+class HolcMembersViewSet(viewsets.ModelViewSet):
+    queryset = models.HolcMembers.objects.all()
+    serializer_class = serializers.HolcMembersSerializer
+
+    @action(detail=False, methods=['POST'])
+    def join_invite_key(self,request):
+        try:
+            holc = models.HolcModel.objects.get(invitation_key=request.data['inviteKey'])
+            user = User.objects.get(username=request.data['user'])
+            models.HolcMembers.objects.create(user=user, holc=holc)
+            members = models.HolcMembers.objects.filter(holc=holc)
+            serializer = self.get_serializer(members, many=True)
+            return Response(serializer.data)
+        except models.HolcModel.DoesNotExist:
+            return Response({"message": "Holc not found."}, status=status.HTTP_404_NOT_FOUND)
+        except models.HolcMembers.DoesNotExist:
+            return Response({"message": "Holc members not found."}, status=status.HTTP_404_NOT_FOUND)
+        except models.MaxMembershipReached:
+            return Response({"message": "Thi Holc has reached its maximum membership and does not accept new candidate!"}, status=status.HTTP_406_NOT_ACCEPTABLE)
