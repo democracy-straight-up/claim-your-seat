2d7c3920ecdf75234f7e14ab95a42a378960a07f
Zaman Ehsani <zamanehsani@gmail.com>
2025-07-29 21:04:06 +0400

    added the holc models to admin panel

--- holc/admin.py
+++ holc/admin.py
@@ -1,3 +1,34 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from holc import models as holcModels
+
+
+class Holc_Admin(admin.ModelAdmin):
+    list_display =['code','invitation_key', 'is_active','member_count', 'district','created_at']
+    list_display_links =['code','invitation_key',  'is_active', 'district','created_at']
+    search_fields =['code','invitation_key','district', 'is_active','created_at']
+admin.site.register(holcModels.HolcModel, Holc_Admin)
+
+class HolcMembers_Admin(admin.ModelAdmin):
+    list_display =['user','holc','is_delegate','is_member','joined_at']
+    list_display_links =['user','holc','is_delegate','is_member','joined_at']
+    search_fields =['user__username','holc__code','is_delegate','is_member','joined_at']
+admin.site.register(holcModels.HolcMembers, HolcMembers_Admin)
+
+class HolcMembersVoteOut_Admin(admin.ModelAdmin):
+    list_display =[ 'voted_at','candidate','holc', 'voter']
+    list_display_links =['voted_at','candidate','holc', 'voter']
+    search_fields =['voted_at','candidate','holc', 'voter']
+admin.site.register(holcModels.VoteOutHolcMember, HolcMembersVoteOut_Admin)
+
+class HolcMembersVoteIn_Admin(admin.ModelAdmin):
+    list_display =[ 'voted_at','candidate','holc', 'voter']
+    list_display_links =['voted_at','candidate','holc', 'voter']
+    search_fields =['voted_at','candidate','holc', 'voter']
+admin.site.register(holcModels.VoteInHolcMember, HolcMembersVoteIn_Admin)
+
+class HolcMembersPutFarward_Admin(admin.ModelAdmin):
+    list_display =[ 'voted_at','candidate', 'holc', 'voter']
+    list_display_links =['voted_at','candidate', 'holc', 'voter']
+    search_fields =['voted_at','candidate', 'holc', 'voter']
+admin.site.register(holcModels.PutFarwardHolcMember, HolcMembersPutFarward_Admin)
