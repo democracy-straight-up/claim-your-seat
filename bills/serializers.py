1774e855a8d3e10f7f9b22bec87245c2ec008250
Zaman Ehsani <zamanehsani@gmail.com>
2025-07-30 22:11:34 +0400

    added the serializer for house rep notes

--- bills/serializers.py
+++ bills/serializers.py
@@ -198,3 +198,34 @@ class BillHolcNotesSerializer(serializers.ModelSerializer):
         if not is_holc:
             raise serializers.ValidationError("Only Holc can create Holc notes")
         return data
+    
+
+class BillHouseRepNotesSerializer(serializers.ModelSerializer):
+    bill = CustomBillSerializer(read_only=True)
+    user = CustomVoterSerializer(read_only=True)
+    bill_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = billModels.BillHouseRepNotes
+        fields = ['id', 'created_at', 'updated_at', 'user', 'bill', 'note', 'bill_id']
+        read_only_fields = ['created_at', 'updated_at', 'user']
+
+    def create(self, validated_data):
+        bill_id = validated_data.pop('bill_id')
+        validated_data['bill_id'] = bill_id
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Validate that the user is a Holc"""
+        from rep.models import DistrictCouncilMembers
+        user = self.context['request'].user
+        
+        # Check if the user is actually a MoDa by looking at ModaMembers records
+        is_house_rep = DistrictCouncilMembers.objects.filter(
+            user=user,
+            is_delegate=True
+        ).exists()
+        
+        if not is_house_rep:
+            raise serializers.ValidationError("Only House Rep can create House Rep notes")
+        return data
