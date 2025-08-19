fee1bc85bca4c851ed0b6edd739e68a4f0081be4
Zaman Ehsani <zamanehsani@gmail.com>
2025-08-19 19:02:36 +0400

    aded print

--- dsu/settings.py
+++ dsu/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS= os.environ.get('ALLOWED_HOSTS', '').split(',')
-
+print("allowed hosts:", ALLOWED_HOSTS)
 # Application definition
 INSTALLED_APPS = [
     'corsheaders',
